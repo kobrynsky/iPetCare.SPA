1349aa663d0a28bd38209e4b26f800c5290dfb47
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2020-03-20 20:59:28 +0100

    homePage added

--- ipetcare.spa/src/App.tsx
+++ ipetcare.spa/src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
 import './index.css'
 import { HomeScreen } from './homePage/homeScreen'
 import { RegisterForm } from './auth/components/registerForm'
 import { LoginForm } from './auth/components/loginForm'
 import { Header } from './pageElements/header'
+import { NotLoggedNavbar } from './pageElements/notLoggedNavbar'
 import { useDispatch } from 'react-redux'
 import { setUser } from './state/userActions'
 import { getUserState } from './utils/localStorageHelper'
 import { UserState } from './state/userReducer'
+import { Grid } from '@material-ui/core'
 
 const App: React.FC = () => {
   const dispatch = useDispatch()
@@ -21,17 +23,27 @@ const App: React.FC = () => {
   }, [])
 
   return (
-    <BrowserRouter>
-      <div>
-        {/* <Header /> */}
-        <h1 className="title">iPetCare</h1>
-        <Switch>
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/login" component={LoginForm} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <Grid container direction="column">
+          <Grid container justify="flex-end">
+            <NotLoggedNavbar />
+          </Grid>
+          <Grid item>
+            <NavLink className="title" to="/">
+              <h1>iPetCare</h1>
+            </NavLink>
+          </Grid>
+          <Grid item>
+            <Switch>
+              <Route path="/" component={HomeScreen} exact />
+              <Route path="/register" component={RegisterForm} />
+              <Route path="/login" component={LoginForm} />
+            </Switch>
+          </Grid>
+        </Grid>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- ipetcare.spa/src/homePage/homeScreen.tsx
+++ ipetcare.spa/src/homePage/homeScreen.tsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './home.css'
+import { Grid } from '@material-ui/core'
+import { ContentCard } from './components/contentCard'
+import { Link } from 'react-router-dom'
 
 export function HomeScreen() {
   return (
     <div>
-      <div>COS TAM</div>
+      <Grid container direction="column">
+        <Grid container direction="row">
+          <Grid item xs={3}>
+            <Grid container alignItems="center" direction="column">
+              <Link className="sideNav" to="/">
+                Główna
+              </Link>
+              <Link className="sideNav" to="/">
+                Co to jest?
+              </Link>
+              <Link className="sideNav" to="/">
+                O nas
+              </Link>
+              <Link className="sideNav" to="/">
+                Kontakt
+              </Link>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Grid container direction="column">
+              <Grid item>
+                <ContentCard
+                  title="O nas"
+                  content="orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                />
+              </Grid>
+              <Grid item>
+                <ContentCard
+                  title="O nas"
+                  content="orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                />
+              </Grid>
+              <Grid item>
+                <ContentCard
+                  title="O nas"
+                  content="asdasdasd asd asd asd asd asd asd asd"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={3} />
+        </Grid>
+      </Grid>
     </div>
   )
 }
