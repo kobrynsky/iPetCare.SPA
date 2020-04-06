08f034b40f5e5780aead01d5e1fe2c360ab36a44
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2020-04-06 21:49:21 +0200

    error pages

--- ipetcare.spa/src/common/errorPages/forbiddenPage.tsx
+++ ipetcare.spa/src/common/errorPages/forbiddenPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import '../../index.css'
+import './error.css'
+import { Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
-export const ForbiddenPage = () => (
-  <div>
-    <h3 className="title">403 Forbidden</h3>
-  </div>
-)
+export const ForbiddenPage = () => {
+  const history = useHistory()
+
+  return (
+    <div className="container">
+      <h3 className="title">403 Forbidden</h3>
+      <h6 className="title">Nie masz odpowiednich uprawanień</h6>
+      <Button
+        variant="contained"
+        onClick={() => {
+          history.goBack()
+        }}
+      >
+        Powrót
+      </Button>
+    </div>
+  )
+}
--- ipetcare.spa/src/common/errorPages/unauthorizedPage.tsx
+++ ipetcare.spa/src/common/errorPages/unauthorizedPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../../index.css'
+import './error.css'
+import { Button } from '@material-ui/core'
+import { NavLink } from 'react-router-dom'
 
 export const UnauthorizedPage = () => (
-  <div>
+  <div className="container">
     <h3 className="title">401 Unauthorized</h3>
     <h6 className="title">Twoja sesja wygasła, zaloguj się ponownie</h6>
+    <Button variant="contained">
+      <NavLink to="login">Zaloguj</NavLink>
+    </Button>
   </div>
 )
