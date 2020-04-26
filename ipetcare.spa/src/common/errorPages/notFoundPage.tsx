180acc30afaf59aed539cb0aa4dbaeb8858cb99d
Paweł Kobryński <pawelkobrynski97@gmail.com>
2020-04-26 18:23:03 +0200

    Added nicely look 404 page not found

--- ipetcare.spa/src/common/errorPages/notFoundPage.tsx
+++ ipetcare.spa/src/common/errorPages/notFoundPage.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import '../../index.css'
 import './error.css'
-import { Button } from '@material-ui/core'
+import { Button, makeStyles, Card, Grid } from '@material-ui/core'
 import { NavLink } from 'react-router-dom'
+import kitty from '../../assets/Cat-404.png'
 
 export const NotFoundPage = () => (
-  <div className="container">
-    <h3 className="title">404 Not Found</h3>
-    <Button variant="contained">
-      <NavLink to="/">Strona Główna</NavLink>
-    </Button>
-  </div>
+  <Grid
+    container
+    direction="row"
+    justify="center"
+    alignItems="center"
+  >
+    <Card style={{ width: "60%", height: "70%", marginTop: "5rem", backgroundColor: "rgba(255, 255, 255, 0.9)" }}>
+      <div className="container">
+        <h3 className="title">404 Not Found</h3>
+        <h4 className="title">Sorry, I cat find that page right meow.</h4 >
+        <Button variant="contained">
+          <NavLink to="/">Strona Główna</NavLink>
+        </Button>
+        <img src={kitty} alt="Logo" style={{ width: "50%", height: "50%", marginBottom: "2rem", marginTop: "2rem" }} />
+      </div>
+    </Card>
+  </Grid>
 )
