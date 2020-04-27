d8c2f7f954070813f46afe6183329d39c66552af
Paweł Kobryński <pawelkobrynski97@gmail.com>
2020-04-27 11:54:24 +0200

    fix search

--- ipetcare.spa/src/features/userSearch/containers/userSearchPage.tsx
+++ ipetcare.spa/src/features/userSearch/containers/userSearchPage.tsx
@@ -93,7 +93,7 @@ const renderOwners = (results: GetSearchResponseDto) => {
         firstName={v.firstName}
         lastName={v.lastName}
         email={v.email}
-        imageUrl="https://i.pinimg.com/originals/02/60/fa/0260fa4827b372bb5884126324d171c1.jpg"
+        imageUrl="https://i.kym-cdn.com/entries/icons/facebook/000/032/280/meme1.jpg"
         placeOfResidence={v.placeOfResidence}
       />
     ))
